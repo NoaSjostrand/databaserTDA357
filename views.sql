910f9ba2ead35a5c902356d7541945f6adc60df8
NoaSjostrand <noasj@chalmers.se>
2023-11-10 18:32:14 +0100

    view 3 påbörjad

--- views.sql
+++ views.sql
@@ -3,11 +3,16 @@ CREATE VIEW BasicInformation AS
         FROM Students LEFT OUTER JOIN StudentBranches ON idnr=student;
 
 CREATE VIEW FinishedCourses AS
-    SELECT student, course, name AS coursename, grade, credits
+    SELECT student, course, name AS courseName, grade, credits
         FROM Taken LEFT OUTER JOIN Courses ON course=code;
 
 
 CREATE VIEW Registrations AS
-    SELECT idnr AS student, course, COALESCE(student, 'waiting') AS status
-        FROM Registered RIGHT OUTER JOIN Students ON student=idnr;
+    (SELECT idnr AS student, course, 'registered' AS status
+        FROM Students JOIN Registered ON idnr=student)
+    UNION
+    (SELECT idnr AS student, course, 'waiting' AS status 
+        FROM Students, Registered
+            WHERE (idnr, course) NOT IN (SELECT student, course FROM Registered));
+
 
