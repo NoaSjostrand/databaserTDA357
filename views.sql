63168e0f99f835befc3797686c059f2dacd627a2
NoaSjostrand <noasj@chalmers.se>
2023-11-10 17:23:43 +0100

    ingen succé

--- views.sql
+++ views.sql
@@ -6,8 +6,8 @@ CREATE VIEW FinishedCourses AS
     SELECT student, course, name AS coursename, grade, credits
         FROM Taken LEFT OUTER JOIN Courses ON course=code;
 
--- # TODO De va lite lurigt
--- CREATE VIEW Registrations AS
---     SELECT idnr AS student, course, COALESCE(student, 'waiting') AS status
---         FROM Students LEFT OUTER JOIN Registered ON idnr=student;
+
+CREATE VIEW Registrations AS
+    SELECT idnr AS student, course, COALESCE(student, 'waiting') AS status
+        FROM Registered RIGHT OUTER JOIN Students ON student=idnr;
 
